3eaa289125b99182dcae32f84dc128d89b095c55
onward-web <developer@onward-web.com>
2023-01-11 21:52:34 +0200

    Update UpdateGeonameByTranslateText.php

--- src/Jobs/UpdateGeonameByTranslateText.php
+++ src/Jobs/UpdateGeonameByTranslateText.php
@@ -5,9 +5,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use MichaelDrennen\Geonames\Models\AlternateName;
 use MichaelDrennen\Geonames\Models\GeonameTranslateText;
-use MichaelDrennen\Geonames\Models\Geoname;
-
 
 class UpdateGeonameByTranslateText //implements ShouldQueue
 {
@@ -18,7 +17,7 @@ class UpdateGeonameByTranslateText //implements ShouldQueue
         GeonameTranslateText::chunkById(100, function ($translateTexts) {
             foreach($translateTexts as $translateText){
 
-                Geoname::where('alternate_name', $translateText->source_text)
+                AlternateName::where('alternate_name', $translateText->source_text)
                     ->where('isolanguage', $translateText->source_lang)
                     ->update(['alternate_name' => $translateText->target_text]);
             }
