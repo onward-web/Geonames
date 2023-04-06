09570f01af2adddca2b5cc82bb657370dca7420c
onward-web <developer@onward-web.com>
2023-04-07 02:27:25 +0300

    Update AlternateNameFilter.php

--- src/ModelFilters/AlternateNameFilter.php
+++ src/ModelFilters/AlternateNameFilter.php
@@ -9,8 +9,15 @@ use EloquentFilter\ModelFilter;
 class AlternateNameFilter  extends ModelFilter
 {
     public function alternateNameContains(string $name){
-        return $this->where('alternate_name', 'like',  $name . '%')
-            ->orWhere('alternate_name_edited', 'like',  $name . '%');
-    }
+        return
+            $this->where(function ($query) use($name) {
+                $query->where('alternate_name_edited', 'like',  $name . '%')
+                    ->orWhere(function ($query) use($name) {
+                        $query->whereNull('alternate_name_edited')
+                            ->where('alternate_name', 'like',  $name . '%');
 
+                    });
+            });
+
+    }
 }
