43788089e2c395d92eb8842e2f924c50deedc0e3
onward-web <developer@onward-web.com>
2023-04-07 06:15:21 +0300

    Create Install.php

---
+++ src/Console/Install.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MichaelDrennen\Geonames\Console;
+use MichaelDrennen\Geonames\Jobs\InstallJob;
+
+class Install extends Command
+{
+    protected $signature = 'geonames:install
+        {--connection= : If you want to specify the name of the database connection you want used.} 
+        {--country=* : Add the 2 digit code for each country. One per option.}      
+        {--language=* : Add the 2 character language code.} 
+        {--storage=geonames : The name of the directory, rooted in the storage_dir() path, where we store all downloaded files.}
+        {--test : Call this boolean switch if you want to install just enough records to test the system. Makes it fast.}';
+
+    public function handle() {
+
+        $jobInstanse = new InstallJob($this->option( 'country' ), $this->option( 'language' ), $this->option( 'storage' ));
+        dispatch($jobInstanse);
+    }
+}
