9f1fa4abf884fa917ba74b5aff15a6ad5ac392cd
onward-web <developer@onward-web.com>
2023-04-07 23:20:38 +0300

    Update AlternateName.php

--- src/Models/AlternateName.php
+++ src/Models/AlternateName.php
@@ -2,9 +2,11 @@
 
 namespace MichaelDrennen\Geonames\Models;
 
+use Carbon\Carbon;
 use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Support\Facades\DB;
 
 class AlternateName extends Model
 {
@@ -20,6 +22,7 @@ class AlternateName extends Model
 
     public $incrementing = false;
 
+    // original field alternateNameId  transform to string, custom use Uuid
     protected $keyType = 'string';
 
     /**
@@ -73,4 +76,56 @@ class AlternateName extends Model
         return $this->alternate_name;
 
     }
+
+    /**
+     * @param int $geonameid
+     * @param string $isolanguage
+     * @param string $alternate_name
+     * @param bool $isPreferredName
+     * @param bool $isShortName
+     * @param $isColloquial
+     * @param $isHistoric
+     * @return void
+     */
+    public function addCustomAlternateNameRecord(int $geonameid, string $isolanguage, string $alternate_name, bool $isPreferredName, bool $isShortName, $isColloquial, $isHistoric ){
+
+        $tmpItem = new self();
+        $table = $tmpItem->getTable();
+        $alternateNameUuid = $tmpItem->newUniqueId();
+
+        $pdo = DB::connection(GEONAMES_CONNECTION)->getPdo();
+        $stmt = $pdo->prepare(
+            'INSERT INTO `' . $table. '` SET
+                                    `alternateNameId` = :alternateNameId,
+                                    `geonameid` = :geonameid,
+                                    `isolanguage` = :isolanguage,
+                                    `alternate_name` = :alternate_name,
+                                    `isPreferredName` = :isPreferredName,
+                                    `isShortName` = :isShortName,
+                                    `isColloquial` = :isColloquial,
+                                    `isHistoric` = :isHistoric, 
+                                    `isCustom` = :isCustom,
+                                    `created_at` = :created_at,
+                                    `updated_at` = :updated_at
+                                '
+        );
+        $stmt->execute(
+            [
+                ':alternateNameId' => (string)$alternateNameUuid,
+                ':geonameid' => (int)$geonameid,
+                ':isolanguage' => (string)$isolanguage,
+                ':alternate_name' => (string)$alternate_name,
+                ':isPreferredName' => (bool)$isPreferredName,
+                ':isShortName' => (bool)$isShortName,
+                ':isColloquial' => (bool)$isColloquial,
+                ':isHistoric' => (bool)$isHistoric,
+                ':isCustom' => 1,
+                ':created_at' => (string)Carbon::now()->format('Y-m-d H:i:s'),
+                ':updated_at' => (string)Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        );
+
+        return $alternateNameUuid;
+    }
+
 }
